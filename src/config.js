d4177a6a33d455db9a3083c68aae5e3249e29c64
Natalie Ayuba <natalieayuba@hotmail.com>
2025-07-17 00:06:13 +0100

    Updated portfolio url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Colourify',
   url: 'mycolourify.netlify.app',
-  portfolio: 'natalieayuba.com',
+  portfolio: 'https://www.natalieayuba.com',
   description:
     'Colorify is a tool that generates a colour palette from the cover art of your top albums on Spotify.',
   image: '%PUBLIC_URL%/images/og-image.png',
